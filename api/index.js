932e291852ab2a81fdc82eb99c7f0d33fffa4fdf
ashishverma2831 <ashishverma510207@gmail.com>
2024-01-05 21:19:33 +0530

    Added error handling middleware and updated the signup route to use it.

--- api/index.js
+++ api/index.js
@@ -27,5 +27,17 @@ app.listen(3000,()=>{
 //         message:'Hello express'
 //     })
 // })
+
+
+//middlewares
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message||"Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    })
+})
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/user.model.js')
 const bcryptjs = require('bcryptjs');
 
-router.post('/signup',async (req,res)=>{
+router.post('/signup',async (req,res,next)=>{
     // console.log(req.body);
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -12,7 +12,8 @@ router.post('/signup',async (req,res)=>{
         await newUser.save();
         res.status(201).json('User Created Successfully')
     } catch (error) {
-        res.status(500).json(error.message)
+        // res.status(500).json(error.message)
+        next(error);
     }
 })
 
